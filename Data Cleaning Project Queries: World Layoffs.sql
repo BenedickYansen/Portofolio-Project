9d43f00f5a020b21cba065dc137df713fdf6a8e6
BenedickYansen <148309757+BenedickYansen@users.noreply.github.com>
2024-11-15 17:01:34 +0700

    Update Data Cleaning Project Queries: World Layoffs.sql

--- Data Cleaning Project Queries: World Layoffs.sql
+++ Data Cleaning Project Queries: World Layoffs.sql
@@ -116,7 +116,7 @@ UPDATE layoffs_staging2
 SET country = TRIM(TRAILING '.' FROM country)
 WHERE country LIKE 'United States%';
 
-SELECT  `date`, STR_TO_DATE(`date`, '%m/%d/%Y')
+SELECT `date`, STR_TO_DATE(`date`, '%m/%d/%Y')
 FROM layoffs_staging2;
 
 UPDATE layoffs_staging2 
@@ -151,21 +151,32 @@ SELECT *
 FROM layoffs_staging2
 WHERE company LIKE 'Bally%';
 
-SELECT  t1.industry, t2.industry
-FROM layoffs_staging2 t1
-JOIN layoffs_staging2 t2 
-	ON t1.company = t2.company
-AND t1.location = t2.location
-WHERE (t1.industry IS NULL OR t1.industry = '')
-AND t2.industry IS NOT NULL;
+SELECT *
+FROM layoffs_staging2
+WHERE company LIKE 'Airbnb%';
 
-UPDATE layoffs_staging2 t1 
-JOIN layoffs_staging2 t2 
-	ON t1.company = t2.company 
-SET t1.industry = t2.industry
-WHERE t1.industry IS NULL
-AND t2.industry IS NOT NULL;
-
+SELECT 
+  t1.industry, 
+  t2.industry 
+FROM 
+  layoffs_staging2 t1 
+  JOIN layoffs_staging2 t2 ON t1.company = t2.company 
+  AND t1.location = t2.location 
+WHERE 
+  (
+    t1.industry IS NULL 
+    OR t1.industry = ''
+  ) 
+  AND t2.industry IS NOT NULL;
+  
+UPDATE 
+  layoffs_staging2 t1 
+  JOIN layoffs_staging2 t2 ON t1.company = t2.company 
+SET 
+  t1.industry = t2.industry 
+WHERE 
+  t1.industry IS NULL 
+  AND t2.industry IS NOT NULL;
 
 
 
